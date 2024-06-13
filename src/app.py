da7eb0bb7c6419708b8777f69026c77dcd5aaaa5
nickStanisic <nicholasstanisic@gmail.com>
2024-06-13 11:59:46 -0600

    added weather calculations and labels to index.html

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, render_template
+from weather import calculatePoints
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template('index.html')
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route("/", methods=['GET','POST'])
+def index():
+    data = None
+    lowTemp = 0
+    highTemp = 0
+    startTime = 0
+    endTime = 0
+    startDate = 0
+    endDate = 0
+    if request.method == 'POST':
+        lowTemp = request.form['lowTemp']
+        highTemp = request.form['highTemp']
+        startTime = request.form['startTime']
+        endTime = request.form['endTime']
+        startDate = request.form['startDate']
+        endDate = request.form['endDate'] 
+        data = calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate)
+    return render_template('index.html', data=data)
