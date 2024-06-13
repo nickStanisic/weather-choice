28ec84924ce495a93534f19ce715db13bf6b0041
nickStanisic <nicholasstanisic@gmail.com>
2024-06-13 12:05:07 -0600

    test

--- src/weather.py
+++ src/weather.py
@@ -30,7 +30,8 @@ def get_temperatures_for_colorado(start, end, lowTemp, highTemp):
 
 def calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate):
     #range determined by date/time
-    start = int(startDate) + int(startTime)
-    end = int(endTime) + int(endDate) + 1
-    returnData = get_temperatures_for_colorado(start,end, lowTemp, highTemp)
-    return returnData
+    return "Hello"
+    # start = int(startDate) + int(startTime)
+    # end = int(endTime) + int(endDate) + 1
+    # returnData = get_temperatures_for_colorado(start,end, lowTemp, highTemp)
+    # return returnData
