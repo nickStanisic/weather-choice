420b719d7cd6805f823bf561838e7489a2599371
nickStanisic <nicholasstanisic@gmail.com>
2024-06-20 08:55:44 -0600

    database with sqllite working

--- src/app.py
+++ src/app.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template
-from src.weather import calculatePoints
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template, jsonify
+from weather import calculatePoints
+from flask_sqlalchemy import SQLAlchemy
+import requests
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from sqlalchemy.sql import exists
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+database_url = os.getenv('DATABASE_URI')
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///Weather.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(database_url, 'sqlite:///local.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
-# db = SQLAlchemy(app)
-# class Weather(db.Model):
-#     wid = db.Column(db.Integer, primary_key = True)
-#     lat = db.Column(db.Float, nullable=False)
-#     lon = db.Column(db.Float, nullable=False)
-#     temperature = db.Column(db.Float, nullable=False)
+class Weather(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    dt = db.Column(db.Integer, nullable = False)
+    lat = db.Column(db.Float, nullable=False)
+    lon = db.Column(db.Float, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Weather {self.dt}>'
+
+@app.before_request
+def create_tables():
+    db.create_all()
+
 
 
 @app.route("/", methods=['GET','POST'])
 def index():
     data = None
+    
+    fetch_weather_data()
     if request.method == 'POST':
         lowTemp = request.form['lowTemp']
         highTemp = request.form['highTemp']
@@ -28,3 +49,43 @@ def index():
         endDate = request.form['endDate'] 
         data = calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate)
     return render_template('index.html', data=data)
+    
+@app.route('/weather', methods=['GET'])
+def get_all_weather_data():
+    weather_data = Weather.query.all()
+    results = [
+        {
+            "dt": weather.dt,
+            "lat": weather.lat,
+            "lon": weather.lon,
+            "temperature": weather.temperature
+        } for weather in weather_data]
+    return jsonify(results)
+
+
+def fetch_weather_data():
+    """Function to fetch weather data from an API and store it in the database."""
+    min_lat = 41
+    lat_increases = 3
+    min_long = -108
+    long_increases = 3
+    units = "imperial"
+
+    for i in range (min_lat,min_lat - lat_increases, -1):
+        for j in range (min_long,min_long + long_increases):
+            response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat={i}&lon={j}&appid={api_key}&units={units}')
+            if response.status_code == 200:
+                data = response.json()
+                for k in range (0,data.get('cnt')):
+                    new_entry = Weather(dt=data.get('list')[k].get("dt"), 
+                                        lat=i, 
+                                        lon=j, 
+                                        temperature=data.get('list')[k].get('main').get('temp'))
+                    db.session.add(new_entry)
+                    db.session.commit()
+            else:
+                print("Failed to fetch data")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/weather.py
+++ src/weather.py
@@ -11,46 +11,15 @@ long_increases = 3
 
 def get_weather_data(lat, lon, API_key, units):
     return requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_key}&units={units}').json()
-    #have function to submit temp and times into database
-    # add_data_to_database(lat, lon, resp)
 
-# def add_data_to_database(lat, lon, response):
-#     for i in range (0,response.get('cnt')):
-#         new_entry = Weather(wid=i, lat=lat, lon=lon, temperature=response.get('list')[i].get('main').get('temp'))
-#         #new_entry = Weather(wid=response.get('list')[i].get("dt"), temperature=response.get('list')[i].get('main').get('temp'))
-#         db.session.add(new_entry)
-#         db.session.commit()
-
-# def ask_data_base(lat, lon, start, end):
-#     for i in range (start, end):
-#         currentTemp = Weather.query.get(i,lat,lon)
-#         print(currentTemp)
-        
-
-def get_temperatures_from_database(start, end, lowTemp, highTemp):
-    colorado_temperature_list = []
-    for i in range (min_lat,min_lat - lat_increases, -1):
-        temps_for_lat = []
-        for j in range (min_long,min_long + long_increases):
-            data = get_weather_data(i,j,api_key,"imperial")
-            withinRange = True
-            for k in range(start, end):
-                currentTemp = data.get('list')[k].get('main').get('temp')
-                if currentTemp < float(lowTemp) or currentTemp > float(highTemp):
-                    withinRange = False
-            temps_for_lat.append(withinRange)
-        colorado_temperature_list.append(temps_for_lat)
-    return colorado_temperature_list
-    
 def get_temperatures_for_colorado(start, end, lowTemp, highTemp):
     colorado_temperature_list = []
     for i in range (min_lat,min_lat - lat_increases, -1):
         temps_for_lat = []
         for j in range (min_long,min_long + long_increases):
-            data = get_weather_data(i,j,api_key,"imperial")
             withinRange = True
             for k in range(start, end):
-                currentTemp = data.get('list')[k].get('main').get('temp')
+                currentTemp = 0 #need to update this to assess db values
                 if currentTemp < float(lowTemp) or currentTemp > float(highTemp):
                     withinRange = False
             temps_for_lat.append(withinRange)
@@ -61,4 +30,4 @@ def calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate):
     #range determined by date/time
     start = int(startDate) + int(startTime)
     end = int(endTime) + int(endDate) + 1
-    return get_temperatures_from_database(start,end,lowTemp,highTemp)
+    return get_temperatures_for_colorado(start,end,lowTemp,highTemp)
